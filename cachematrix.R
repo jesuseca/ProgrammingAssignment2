84bf2f49afe7a96329a58698d9f7991ec22675ba
jesuseca <39555499+jesuseca@users.noreply.github.com>
2018-06-11 09:44:40 -0500

    Implementation of functionality
    The following functions allow us to calculate the inverse of a square matrix and efficiently use resources by storing the inverse matrix in memory and invoke it whenever necessary, as well as recalculate its inverse if the square matrix has changed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions allow us to calculate the inverse of a square matrix and efficiently 
+## use resources by storing the inverse matrix in memory and invoke it whenever necessary, as well as
+## recalculate its inverse if the square matrix has changed.
 
-## Write a short comment describing this function
+
+## Create a special matrix, which manages its original and inverse values by storing both values in memory
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    solx <- NULL
+    set <- function(y) {
+        x <<- y
+        solx <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) solx <<- solve
+    getsolve <- function() solx
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+ 
+## Return the inverse matrix of x, if it already exists, takes it from memory otherwise calculate it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    solx <- x$getsolve()
+    if(!is.null(solx)) {
+        message("getting cached data")
+        return(solx)
+    }
+    data <- x$get()
+    solx <- solve(data, ...)
+    x$setsolve(solx)
+    solx
 }
